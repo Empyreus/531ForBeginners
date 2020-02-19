e870ae762c09bd7d7d80e20b6ed121911d71aef0
RJ <rjsouza1995@gmail.com>
2020-02-19 13:23:55 -0800

    Initialize Flutter Project

---
+++ android/app/src/main/kotlin/empyreandesign/for_beginners_531/MainActivity.kt
@@ -0,0 +1,12 @@
+package empyreandesign.for_beginners_531
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
