c5e002cc5d078bea94684ced989c7f212f4cb55d
RJ <rjsouza1995@gmail.com>
2020-05-03 12:34:25 -0700

    Init Weight Display

---
+++ lib/screens/home/reps_page.dart
@@ -0,0 +1,35 @@
+import 'package:for_beginners_531/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:for_beginners_531/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:for_beginners_531/screens/home/weight_list.dart';
+import 'package:for_beginners_531/models/weight.dart';
+
+class Reps extends StatelessWidget {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<List<Weight>>.value(
+      value: DatabaseService().weights,
+      child: Scaffold(
+          backgroundColor: Colors.grey[300],
+          appBar: AppBar(
+            title: Text('531 For Beginners'),
+            backgroundColor: Colors.blueGrey[400],
+            elevation: 0.0,
+            actions: <Widget>[
+              FlatButton.icon(
+                icon: Icon(Icons.person),
+                label: Text('logout'),
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+              ),
+            ],
+          ),
+          body: Text("REPS")
+          ),
+    );
+  }
+}
---
+++ lib/screens/home/reps_table.dart
@@ -0,0 +1,11 @@
+class RepsTable extends StatefulWidget {
+  @override
+  _RepsTableState createState() => _RepsTableState();
+}
+
+class _RepsTableState extends State<RepsTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
